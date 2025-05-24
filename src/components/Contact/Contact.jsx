52e4ff52c3d0774de8e975ef4dc0866d35e202ee
jasmeen786 <148907683+jasmeen786@users.noreply.github.com>
2025-05-23 19:13:32 -0600

    Fixed build errors and cleaned up

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:Jas2004meen@gmail.com">Jas2004meen@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a
+            href="https://www.linkedin.com/in/jasmeen-b20384298/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            linkedin.com/in/jasmeen-b20384298
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/githubIcon.png")}
+            alt="GitHub icon"
+          />
+          <a
+            href="https://github.com/jasmeen786"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.com/Jasmeen-code-space.
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
